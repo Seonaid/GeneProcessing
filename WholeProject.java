7dfff4c0fa184cbd043d0e895a947fbd2980a179
Seonaid Lee <seonaidl@gmail.com>
2019-08-06 13:49:11 -0300

    additional test cases for findSingleGene

--- WholeProject.java
+++ WholeProject.java
@@ -10,22 +10,69 @@
  * @version (a version number or a date)
  */
 public class WholeProject {
-    public String findSimpleGene(String dna, String startCodon, String stopCodon){
-        return "";
+    public String findSingleGene(String dna, String startCodon, String stopCodon){
+        String result = "";
+        int startIndex = dna.toUpperCase().indexOf(startCodon.toUpperCase());
+        int stopIndex = dna.length();
+        
+        // If the startCodon is not found in the dna string, there is no gene
+        if (startIndex == -1){
+            return result;
+        }
+        
+        stopIndex = dna.toUpperCase().indexOf(stopCodon.toUpperCase(), startIndex+3);
+        if (stopIndex == -1){
+            return result; // if there is no stop codon, there is no gene
+        }       
+        
+        result = dna.substring(startIndex, stopIndex+3);
+        return result;
     }
     
-    public void testFindSimpleGene(){
+    public void testFindSingleGene(){
+        System.out.println("\nStart testFindSingleGene");
         String testDNA = "";
         String testGene = "";
         String startCodon = "ATG";
         String stopCodon = "TAA";
-        // Case 1: startCodon-stopCodon, no intermediate code
-        testDNA = "ATGTAA";
-        testGene = findSimpleGene(testDNA, startCodon, stopCodon);
-        if (testGene != "ATGTAA") System.out.println("Cannot find basic gene: " + testDNA);
         
-        // Case2: works with lower case dna
-        // Case3: works with lower case start and stop codons
+        // Case: no start codon = no gene
+        testDNA = "TAACCGTGA";
+        testGene = findSingleGene(testDNA, startCodon, stopCodon);
+        if (!testGene.equals("")) System.out.println("Should not find gene without startCodon: " + testDNA);
+        
+        // Case: startCodon-stopCodon, no intermediate code
+        testDNA = "ATGTAA";
+        testGene = findSingleGene(testDNA, startCodon, stopCodon);
+        if (!testGene.equals("ATGTAA")) System.out.println("Cannot find basic gene: " + testDNA);
+        
+        // Case: Finds gene with three codons
+        testDNA = "ATGCGATAA";
+        testGene = findSingleGene(testDNA, startCodon, stopCodon);
+        if (!testGene.equals("ATGCGATAA")) System.out.println("Cannot find three codon gene: " + testDNA);        
+        
+        // Case: Finds gene with trailing codons
+        testDNA = "ATGCGATAACCG";
+        testGene = findSingleGene(testDNA, startCodon, stopCodon);
+        if (!testGene.equals("ATGCGATAA")) System.out.println("Trailing codons: " + testGene);        
+
+        // Case: works with lower case dna
+        testDNA = "atgcgataaccg";
+        testGene = findSingleGene(testDNA, startCodon, stopCodon);
+        if (!testGene.equals("atgcgataa")) System.out.println("Lower case dna: " + testGene);              
+
+        // Case: works with lower case start codon
+        startCodon = "atg";
+        testGene = findSingleGene(testDNA, startCodon, stopCodon);
+        if (!testGene.equals("atgcgataa")) System.out.println("Lower case startCodon: " + testGene);
+        
+        // Case: works with "TAG" stop codon
+        stopCodon = "TAG";
+        testDNA = "atgcgatagccg";        
+        testGene = findSingleGene(testDNA, startCodon, stopCodon);
+        if (!testGene.equals("atgcgatag")) System.out.println("Change stopCodon: " + testGene);        
+        
+        System.out.println("All tests run FindSingleGene");
         
     }
 }
